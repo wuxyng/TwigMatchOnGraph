d4a865de93bff828e0a0fb51de6b540ec93b061e
wuxyng <xiaoying.wu@gmail.com>
2022-06-28 13:54:39 +0800

    Add files via upload

---
+++ Scripts/tsdsspi.sh
@@ -0,0 +1,5 @@
+cd /home/work/TwigStackDSSPI
+export CLASSPATH=./bin:/home/work/JARs/commons-math3-3.6.jar:/home/JARs/algs4.jar:/home/work/JARs/dtdparser121.jar:/home/work/JARs/java_cup_11.jar
+
+java graph.main.TwigStackDMain ep_lb20.dag inst_lb20_tree_c.qry
+
